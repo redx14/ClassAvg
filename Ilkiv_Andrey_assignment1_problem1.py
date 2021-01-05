145b2badb420f0d60c5521d9c10d6fdccd9a9e73
Andrey <55301405+redx14@users.noreply.github.com>
2021-01-04 21:55:48 -0500

    Added file

---
+++ Ilkiv_Andrey_assignment1_problem1.py
@@ -0,0 +1,8 @@
+Classname = "Introduction to Computer Programming"
+TestOneAvg = 95
+TestTwoAvg = 76
+TestThreeAvg = 87
+
+TestAvgs = (TestOneAvg + TestTwoAvg + TestThreeAvg)/3
+print(Classname)
+print("The overall test average is :" , TestAvgs)
